3b40749bebf60974c3f0459e93dd80efcc629880
yibianqingchen <sj_18cl@163.com>
2019-04-02 17:31:46 +0800

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+package.com.workspace.shenqian.helloworld;
+public class HelloWorld               
+{
+    public String sayHello(){
+        return "Hello World";
+    }
+    public stacic void main(String[] args){
+        System.out.print( new HelloWorld().sayHello());
+    }
+}
